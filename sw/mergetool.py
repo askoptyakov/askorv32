aea41c03cf94383bfbcfb4eeb853db61f0c09dd2
lokirson <rv9can2016@gmail.com>
2024-09-25 21:04:23 +0500

    Закончил READMI, дописал коментарии

--- sw/mergetool.py
+++ sw/mergetool.py
@@ -1,5 +1,4 @@
 import re
-
 #0.1 Соотносим блоки BSRAM и начальные позиции записи в строке *.fs
               #    0    1    2    3    4    5    6    7   8   9  10
 bsram_stPosR10 = [2570,2390,2210,2030,1850,1670,1490,950,770,590,410]
@@ -28,8 +27,7 @@ i = 0
 j = 0
 for i in range(4):
     for j in range(len(bsram[i]), 2048):                                                            #Дополняем строки до полного объёма блоков BSRAM
-        bsram[i].append(0)
-
+        bsram[i].append(0)                                                                          #Добавляем в массив 
 #3 Анализируем файл размещения BSRAM в плис *.posp
 with open("hw/impl/pnr/riscv.posp", "r") as file:                                                   #Открываем *.posp
     text_data = file.read()
@@ -44,7 +42,7 @@ for string in imem_list:
 for i , string in enumerate(bsram_loc):                                                             #Удаляем все символы из строк и преобразовываем строки в числа
     for j, element in enumerate(string):
         if (j == 1):                                                                                #Если элемент 1, то происходит замена, на:                                                                           
-            match element:
+            match element:                                                                          #Свитч R10, R28
                 case 'R10': bsram_loc[i][j] = 0                                                     #Заменяем R10 на 0
                 case 'R28': bsram_loc[i][j] = 1                                                     #Заменяем R28 на 1
                 case _: print(element,'Ошибка! Сектор BSRAM не существует!')                        #Несуществующий сектор BSRAM
@@ -53,7 +51,6 @@ bsram_loc.sort()                                                                
 #4 Подменяем нужные фрагменты в файле *.fs
 with open("hw/impl/pnr/ao_0.fs", "r") as file:                                                      #Открываем *.fs и выгрузим все строки отдельно в список
     conf_data = file.readlines()
-
 for bsram_num in range(4):                                                                          #Сначала определим какой блок BSRAM и стартовый адрес записи
     bsram_zone = bsram_loc[bsram_num][1]                                                            #Зона в которой располагается BSRAM: 0 - R10; 1 - R28;       
     bsram_no   = bsram_loc[bsram_num][2]                                                            #Номер блока BSRAM в который записываем
@@ -67,37 +64,34 @@ for bsram_num in range(4):                                                      
         second_part_of_str = ''.join(bsram_str)                                                     #Изменяем вторую часть строки на "0" для дальнейшей обработки
         third_part_of_str = conf_data[current_str][base_pos+1:]                                     #Оставляем без измененной третью часть строки
         conf_data[current_str] = first_part_of_str + second_part_of_str + third_part_of_str         #Собираем все части строк в одну
-    bit_list0 = []                                                                                  
-    bit_list1 = []
-    bit_list = [] 
-    for i, byte in enumerate(bsram[bsram_num]):
-        each2bytes = i%2
+    bit_list0 = []                                                                                  #Младший бит                                                                                  
+    bit_list1 = []                                                                                  #Старший бит
+    bit_list = []                                                                                   #Переменая из двух бит
+    for i, byte in enumerate(bsram[bsram_num]):                                                     #Перебераем файл .bin для создания файла .fs
+        each2bytes = i%2                                                                            #Каждые два байта
         if (each2bytes):                                                                            #Создаём список с битами 2ух байт для записи в строку BSRAM
-            bit_list1 = [1 if x=='1' else 0 for x in "{:08b}".format(byte)]
-            bit_list = bit_list1 + bit_list0
+            bit_list1 = [1 if x=='1' else 0 for x in "{:08b}".format(byte)]                         #Упращеная запись условия обернутого в цикл
+            bit_list = bit_list1 + bit_list0                                                        #Собираем два байта в один лист
         else:    
-            bit_list0 = [1 if x=='1' else 0 for x in "{:08b}".format(byte)]
+            bit_list0 = [1 if x=='1' else 0 for x in "{:08b}".format(byte)]                         #Упращеная запись условия обернутого в цикл
         if (each2bytes):                                                                            #Расставляем битики данных в одной строке BSRAM
             j = i//2                                                                                #Счётчик пар байтов
             p = j//256                                                                              #Номер прохода (всего 4 прохода)
             if((j % 256) == 0): shift_str = 0                                                       #Осуществляем сброс смещения строки каждые 256 значений
-            current_str = base_str + shift_str
+            current_str = base_str + shift_str                                                      #Добовляем смещение текущей строки
             bsram_str = list(conf_data[current_str][base_pos-154:base_pos+1])                       #Берём за основу строку из файла и заменяем в ней нужные символы
-            for k, bit in enumerate(bit_list):
-                bsram_str[154-bit_loc_pas[p][k]] = str(bit)
-        
-        #Записываем битики в строку BSRAM выгруженную из файла *.fs
+            for k, bit in enumerate(bit_list):                                                      #Перебераем массив bit_list                                                                                                             
+                bsram_str[154-bit_loc_pas[p][k]] = str(bit)                                         #Создаем строку bsram_str из bit_list                         
+            #Записываем битики в строку BSRAM выгруженную из файла *.fs
             first_part_of_str = conf_data[current_str][:base_pos-154]                               #Оставляем без измененной первую часть строки
             second_part_of_str = ''.join(bsram_str)                                                 #Изменяем вторую часть строки (только в ней хронятся полезные данные)
             third_part_of_str = conf_data[current_str][base_pos+1:]                                 #Оставляем без измененной третью часть строки
             conf_data[current_str] = first_part_of_str + second_part_of_str + third_part_of_str     #Собираем 3 части строки в одну
-            #Определяем номер строки для следующей записи
-            match ((j % 256) % 4):                                                                  #Всего 4 прохода по 256 строк          
+            match ((j % 256) % 4):                                                                  #Определяем номер строки для следующей записи (всего 4 прохода)   
                 case 0: shift_str = shift_str - 128                                                 #Начальная позиция для первого прохода
                 case 1: shift_str = shift_str + 64                                                  #Начальная позиция для второго прохода
                 case 2: shift_str = shift_str - 128                                                 #Начальная позиция для третьего прохода
                 case 3: shift_str = shift_str + 191                                                 #Начальная позиция для четвертого прохода
-
     print('Блок BSRAM', bsram_num, 'записан!')   
 with open("hw/impl/pnr/riscv.fs", "w") as file:                                                     #Запись данных в новый файл riscv.fs
     file.writelines(conf_data)
